dd9ec7b06fef2d7455ea30a1cd7020c830084acb
Zainab Aamir <zaamir427@gmail.com>
2025-01-27 23:35:30 +0500

    data scraping

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ url = requests.get('https://books.toscrape.com/').text
 soup = BeautifulSoup(url, 'lxml')
 
 products = soup.find('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
-article = products.find('article', class_ = 'product_pod')
-print(article)
+article = products.find('article', class_ = 'product_pod').text.strip()
+clean_text = ' '.join(article.split()) 
+print(clean_text)
+
