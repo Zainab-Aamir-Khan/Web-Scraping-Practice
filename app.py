9c28c24c4d5b499af3aaf65c90c77ce914c4a2be
Zainab Aamir <zaamir427@gmail.com>
2025-01-28 00:11:25 +0500

    scraped the book names

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ soup = BeautifulSoup(url, 'lxml')
 products = soup.find('ol', class_ = 'row')
 
 
-book = products.find('h3').a['title']
-print(book)
-
+bookItems = products.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+for bookTitle in bookItems:
+    book = bookTitle.h3.a['title']
+    print(book)
 
