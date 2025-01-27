27dc49b40a9fe764488e1a690a802f1d91a7e21d
Zainab Aamir <zaamir427@gmail.com>
2025-01-27 21:07:19 +0500

    Web Scraping practice

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ with open('index.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
 
-mainTitle = soup.find_all('div', class_ = 'article')
-    
-print(mainTitle.text)
+div = soup.find('div', class_ = 'article')
+print(div.text)
 
+heading = div.h2.a
+print(heading.text)
 
