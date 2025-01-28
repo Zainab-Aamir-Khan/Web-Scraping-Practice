dea3de00b665608d06d3c9603d05dbb98d4867ef
Zainab Aamir <zaamir427@gmail.com>
2025-01-28 10:22:31 +0500

    Formatted manner

--- app.py
+++ app.py
@@ -4,37 +4,20 @@ import requests
 url = requests.get('https://books.toscrape.com/').text
 soup = BeautifulSoup(url, 'lxml')
 
-products = soup.find('ol', class_ = 'row')
+content = soup.find('ol', class_ = 'row')
 
-#scraping all the headings/title of the books
-content = soup.find('body')
-heading = content.find_all('h3')
-for headings in heading:
-    allHeadings = headings.text
-    print(allHeadings)
+for allContent in content.find_all('li'):
 
+    heading = allContent.h3.text
+    print(heading)
 
-# bookItems = products.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
-# for bookTitle in bookItems:
-#     book = bookTitle.h3.a['title']
-#     print(book)
+    prices = allContent.find('p', class_ = 'price_color').text
+    print(prices)
 
+    stock = allContent.find('p', class_ = 'instock availability').text.strip()
+    print(stock)
 
-#scraping all the prices of the books
-price = content.find_all('p', class_ = 'price_color')
-for prices in price:
-    allPrices = prices.text
-    print(allPrices)
+    form = allContent.find('button', class_ = 'btn btn-primary btn-block').text.strip()
+    print(form)
 
 
-#scraping the status of stock
-stock = content.find_all('p', class_ = 'instock availability')
-for stocks in stock:
-    allStocks = stocks.text.strip()
-    print(allStocks)
-
-
-#scraping the form status
-form = content.find_all('button', class_ = 'btn btn-primary btn-block')
-for forms in form:
-    print(forms.text)
