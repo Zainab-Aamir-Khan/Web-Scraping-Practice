9f9dd103c83ce96f4097148f4225d85cd6013c55
Zainab Aamir <zaamir427@gmail.com>
2025-01-28 09:35:56 +0500

    extracted headings

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ url = requests.get('https://books.toscrape.com/').text
 soup = BeautifulSoup(url, 'lxml')
 
 products = soup.find('ol', class_ = 'row')
+content = soup.find('body')
+heading = content.find_all('h3')
+for headings in heading:
+    allHeadings = headings.text
+    print(allHeadings)
 
 
-bookItems = products.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
-for bookTitle in bookItems:
-    book = bookTitle.h3.a['title']
-    print(book)
+# bookItems = products.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+# for bookTitle in bookItems:
+#     book = bookTitle.h3.a['title']
+#     print(book)
+
 
